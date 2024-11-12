9c7da4342af23087264de5750032955157603860
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-11 20:52:24 -0500

    Equipment & Item Classes

---
+++ ConsoleRpgEntities/Models/Equipment.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleRpgEntities.Models.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpgEntities.Models
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+        public int? WeaponId {  get; set; }
+        public int? ArmorId { get; set; }
+
+        // Navigation properties
+        public virtual Item? Weapon { get; set; }
+        public virtual Item? Armor { get; set; }
+    }
+}
