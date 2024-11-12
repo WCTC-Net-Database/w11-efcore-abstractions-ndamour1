ac14e38519a2cb213755400b1fa6ddb6282d5cae
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-11 21:10:21 -0500

    Update Goblin.cs

--- ConsoleRpgEntities/Models/Characters/Monsters/Goblin.cs
+++ ConsoleRpgEntities/Models/Characters/Monsters/Goblin.cs
@@ -8,8 +8,41 @@ namespace ConsoleRpgEntities.Models.Characters.Monsters
 
         public override void Attack(ITargetable target)
         {
-            // Goblin-specific attack logic
-            Console.WriteLine($"{Name} sneaks up and attacks {target.Name}!");
+            {
+                // "Dice roll"
+                Random rand = new Random();
+                int roll = rand.Next(1, 7);
+        
+                // Equipment stats
+                int attack = 0;
+                int defense = 0;
+        
+                // Damage calculations
+                decimal damage = (roll + attack) / ((2 + 100) / 100);
+        
+                // Total damage
+                int totalDamage = (int)Math.Round(damage, MidpointRounding.AwayFromZero);
+                int overkill = totalDamage + (target.Health - totalDamage);
+        
+                // Goblin-specific attack logic
+                Console.WriteLine($"{Name} sneaks up and attacks {target.Name}!");
+                target.Health -= totalDamage;
+                if (target.Health > 0)
+                {
+                    Console.WriteLine($"{target.Name} took {totalDamage} damage.\n{target.Name}'s HP is at {target.Health}.");
+                }
+                else
+                {
+                    if (target.Health - totalDamage < 0)
+                    {
+                        Console.WriteLine($"{target.Name} took {overkill} damage.\n{target.Name} has been defeated.");
+                    }
+                    else if (target.Health - totalDamage == 0)
+                    {
+                        Console.WriteLine($"{target.Name} took {totalDamage} damage.\n{target.Name} has been defeated.");
+                    }
+                }
+            }
         }
     }
 }
