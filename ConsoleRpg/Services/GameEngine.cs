d22dbc7d55f47c4bc0e2273f8819294e552b907d
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-11 20:29:00 -0500

    Update GameEngine.cs

--- ConsoleRpg/Services/GameEngine.cs
+++ ConsoleRpg/Services/GameEngine.cs
@@ -33,40 +33,88 @@ public class GameEngine
     private void GameLoop()
     {
         _outputManager.Clear();
+        int restorePlayerHealth = _context.Players.OfType<Player>().FirstOrDefault().Health;
+        int restoreGoblinHealth = _context.Monsters.OfType<Goblin>().FirstOrDefault().Health;
 
         while (true)
         {
-            _outputManager.WriteLine("Choose an action:", ConsoleColor.Cyan);
-            _outputManager.WriteLine("1. Attack");
-            _outputManager.WriteLine("2. Quit");
-
-            _outputManager.Display();
-
-            var input = Console.ReadLine();
-
-            switch (input)
+            if (_player.Health > 0 && _goblin.Health > 0)
             {
-                case "1":
-                    AttackCharacter();
-                    break;
-                case "2":
-                    _outputManager.WriteLine("Exiting game...", ConsoleColor.Red);
-                    _outputManager.Display();
-                    Environment.Exit(0);
-                    break;
-                default:
-                    _outputManager.WriteLine("Invalid selection. Please choose 1.", ConsoleColor.Red);
-                    break;
+                _outputManager.WriteLine("Choose an action:", ConsoleColor.Cyan);
+                _outputManager.WriteLine("1. Attack");
+                _outputManager.WriteLine("2. Quit");
+
+                _outputManager.Display();
+
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        AttackCharacter();
+                        break;
+                    case "2":
+                        _outputManager.WriteLine("Exiting game...", ConsoleColor.Red);
+                        _outputManager.Display();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        _outputManager.WriteLine("Invalid selection. Please choose 1.", ConsoleColor.Red);
+                        break;
+                }
+            }
+            else
+            {
+                if (_player.Health > 0 && _goblin.Health <= 0)
+                {
+                    _outputManager.WriteLine($"{_player.Name} wins!", ConsoleColor.Yellow);
+                }
+                else if (_player.Health <= 0 && _goblin.Health > 0)
+                {
+                    _outputManager.WriteLine($"{_player.Name} has fallen in battle.", ConsoleColor.Gray);
+                }
+
+                _outputManager.WriteLine($"Would you like to play again?");
+                _outputManager.WriteLine("1. Yes");
+                _outputManager.WriteLine("2. No");
+                _outputManager.Display();
+
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        _outputManager.Clear();
+                        _player.Health = restorePlayerHealth;
+                        _goblin.Health = restoreGoblinHealth;
+                        break;
+                    case "2":
+                        _outputManager.WriteLine("Exiting game...", ConsoleColor.Red);
+                        _outputManager.Display();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        _outputManager.WriteLine("Invalid selection. Please choose a valid option.", ConsoleColor.Red);
+                        break;
+                }
             }
         }
     }
 
     private void AttackCharacter()
     {
-        if (_goblin is ITargetable targetableGoblin)
+        _outputManager.Clear();
+        if (_goblin is ITargetable targetableGoblin && targetableGoblin.Health > 0)
         {
             _player.Attack(targetableGoblin);
-            _player.UseAbility(_player.Abilities.First(), targetableGoblin);
+            if (targetableGoblin.Health > 0)
+            {
+                _player.UseAbility(_player.Abilities.First(), targetableGoblin);
+                if (_player is ITargetable targetablePlayer && _player.Health > 0)
+                {
+                    _goblin.Attack(targetablePlayer);
+                }
+            }
         }
     }
 
@@ -87,5 +135,4 @@ public class GameEngine
     {
         _goblin = _context.Monsters.OfType<Goblin>().FirstOrDefault();
     }
-
 }
