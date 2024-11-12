72fcd2dbecfeff16bc8889fc06edd205b173df0d
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-11-11 21:05:20 -0500

    Update Player.cs

--- ConsoleRpgEntities/Models/Characters/Player.cs
+++ ConsoleRpgEntities/Models/Characters/Player.cs
@@ -12,21 +12,40 @@ namespace ConsoleRpgEntities.Models.Characters
         public int Health { get; set; }
         public virtual IEnumerable<Ability> Abilities { get; set; }
 
-        public void Attack(ITargetable target)
+        public async void Attack(ITargetable target)
         {
+            // "Dice roll"
+            Random rand = new Random();
+            int roll = rand.Next(1, 7);
+        
+            // Equipment stats
+            int attack = 3;
+            int defense = 2;
+        
+            // Damage calculations
+            decimal damage = (roll + attack) / ((0 + 100) / 100);
+        
+            // Total damage
+            int totalDamage = (int)Math.Round(damage, MidpointRounding.AwayFromZero);
+            int overkill = totalDamage + (target.Health - totalDamage);
+        
             // Player-specific attack logic
             Console.WriteLine($"{Name} attacks {target.Name} with a sword!");
-        }
-
-        public void UseAbility(IAbility ability, ITargetable target)
-        {
-            if (Abilities.Contains(ability))
+            target.Health -= totalDamage;
+            if (target.Health > 0)
             {
-                ability.Activate(this, target);
+                Console.WriteLine($"{target.Name} took {totalDamage} damage.");
             }
             else
             {
-                Console.WriteLine($"{Name} does not have the ability {ability.Name}!");
+                if (target.Health - totalDamage < 0)
+                {
+                    Console.WriteLine($"{target.Name} took {overkill} damage.\n{target.Name} has been defeated.");
+                }
+                else if (target.Health - totalDamage == 0)
+                {
+                    Console.WriteLine($"{target.Name} took {totalDamage} damage.\n{target.Name} has been defeated.");
+                }
             }
         }
     }
